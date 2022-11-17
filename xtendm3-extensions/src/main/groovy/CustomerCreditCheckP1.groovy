88f6ad8da75cc12d9dc9bd53840c9720aca6fcc0
hchotrani-atlantic <hchotrani@atlantic-technologies.com>
2022-11-17 13:43:50 +0100

    Final Commit after project setup

--- xtendm3-extensions/src/main/groovy/CustomerCreditCheckP1.groovy
+++ xtendm3-extensions/src/main/groovy/CustomerCreditCheckP1.groovy
@@ -1,7 +1,7 @@
 class CustomerCreditCheckP1 extends ExtendM3Trigger {
   private final SessionAPI session
   private final InteractiveAPI interactive
-
+ 
   CustomerCreditCheckP1(SessionAPI session, InteractiveAPI interactive) {
     this.session = session
     this.interactive = interactive
